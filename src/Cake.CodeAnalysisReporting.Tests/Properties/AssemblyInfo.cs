e931b983f969f3d12061d679e224f46cf1194032
Pascal Berger <pascal.berger@gmail.com>
2018-03-11 20:48:23 +0100

    Update year from copyright

--- src/Cake.CodeAnalysisReporting.Tests/Properties/AssemblyInfo.cs
+++ src/Cake.CodeAnalysisReporting.Tests/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Cake.CodeAnalysisReporting.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © BBT Software AG, Root/Zermatt, Switzerland")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
